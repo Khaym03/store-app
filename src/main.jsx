75481f5b4d713154e2d539499df317f37c634532
Khaym03 <daniellarosa20003@gmail.com>
2023-12-18 09:18:39 -0400

    added material design css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './css/colors.module.css'
+import './css/tokens.css'
+import './css/theme.css'
+import './css/typography.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
