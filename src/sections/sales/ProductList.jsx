e5cb3105d0b2ac20029d9ae84712cdc478d9d4bc
Khaym03 <daniellarosa20003@gmail.com>
2024-04-30 14:13:49 -0400

    productList refactor

--- src/sections/sales/ProductList.jsx
+++ src/sections/sales/ProductList.jsx
@@ -7,27 +7,53 @@ import { TbBottleFilled } from 'react-icons/tb'
 import { TbBottle } from 'react-icons/tb'
 import { borderColor } from '../../utils'
 import { MdOutlinePlusOne } from 'react-icons/md'
+import { MdAdd } from 'react-icons/md'
 
-const ProductCard = ({ name, value, color }) => {
+const NewProduct = ({ animation }) => {
+  return (
+    <animated.div
+      style={animation}
+      className="bg-slate-100 rounded-md border-2 border-slate-200 text-slate-400 cursor-pointer grid place-items-center transition"
+    >
+      <MdAdd size={'2rem'} />
+    </animated.div>
+  )
+}
+
+NewProduct.propTypes = {
+  animation: PropTypes.object
+}
+
+const ProductCard = ({ name, value, color, animation }) => {
   const transparent = ['cloro', 'acondicionador', 'cera'].includes(name)
   const [isIn, setIsIn] = useState(false)
-  const { orders, setOrders, setTriggerProcessOrders,setSelected, selected } =
+  const { orders, setOrders, setTriggerProcessOrders, setSelected, selected } =
     useContext(ManagerContext)
 
   const enterHandler = () => setIsIn(true)
   const outHandler = () => setIsIn(false)
   const clickHandler = e => {
     e.stopPropagation()
-    if(selected) setSelected(null)
+    if (selected) setSelected(null)
     setOrders([...orders, [name, 1000, value]])
     setTriggerProcessOrders(prev => !prev)
   }
 
+  const selectHandler = ({ name, price }) => {
+    setSelected([name, price])
+  }
+
   return (
-    <div
-      className={`bg-white overflow-hidden grid place-items-center capitalize rounded-lg ProductCard h-full font-medium transition-colors relative p-4`}
+    <animated.div
+      style={animation}
+      className={`bg-white overflow-hidden grid place-items-center capitalize rounded-lg ProductCard h-full font-medium transition relative p-4 border-2 ${
+        selected && selected[0] === name
+          ? `shadow-md ${borderColor(name)} -translate-y-1`
+          : 'border-slate-200 hover:border-slate-500 '
+      }`}
       onMouseEnter={enterHandler}
       onMouseLeave={outHandler}
+      onClick={() => selectHandler({ name, price: value })}
     >
       <span
         className={`grid place-items-center w-full h-full rounded-md ${color}`}
@@ -52,51 +78,49 @@ const ProductCard = ({ name, value, color }) => {
       >
         <MdOutlinePlusOne size={'1.5rem'} />
       </span>
-    </div>
+    </animated.div>
   )
 }
 
 ProductCard.propTypes = {
   name: PropTypes.string,
   value: PropTypes.number,
-  color: PropTypes.string
+  color: PropTypes.string,
+  animation: PropTypes.object
 }
 
 function ProductList() {
-  const { selected, setSelected } = useContext(ManagerContext)
-
-  const transitions = useTransition(listOfProducts, {
+  const plusEmpty = [
+    ...listOfProducts,
+    {
+      name: '',
+      price: 0,
+      color: ''
+    }
+  ]
+  const transitions = useTransition(plusEmpty, {
     from: { scale: 0, opacity: 0 },
     enter: { scale: 1, opacity: 1 },
     config: { duration: 30 },
-    trail: 15,
+    trail: 25,
     keys: item => item.name // Use the product name as the key
   })
-
-  const clickHandler = ({ name, price }) => {
-    setSelected([name, price])
-  }
 
   return (
     <div className="ProductList row-span-3">
       <ul className="grid grid-cols-2 auto-rows-fr border-r border-slate-200 h-full list-none overflow-y-auto p-4 gap-2">
-        {transitions((styles, product) => (
-          <animated.li
-            style={styles}
-            className={`border rounded-md  ProductList transition cursor-pointer  text-slate-600   ${
-              selected && selected[0] === product.name
-                ? `shadow-md ${borderColor(product.name)}`
-                : 'border-slate-200 hover:border-slate-400'
-            }`}
-            onClick={() => clickHandler(product)}
-          >
+        {transitions((animation, product) =>
+          product === plusEmpty.at(-1) ? (
+            <NewProduct animation={animation} />
+          ) : (
             <ProductCard
               name={product.name}
               value={product.price}
               color={product.color}
+              animation={animation}
             />
-          </animated.li>
-        ))}
+          )
+        )}
       </ul>
     </div>
   )
